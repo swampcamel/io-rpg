afed1193ec6bd256b377c7721f04eba35e066517
Ben Kiggen and Dan Mace <ben@>
2018-10-03 16:34:04 -0700

    fix battle system, add graphics, etc

--- src/battle.js
+++ src/battle.js
@@ -1,35 +1,38 @@
-
+import $ from 'jquery';
 export function battleStep (character, target) {
-
 
   let rollToHit = Math.random();
   let dodgeAttempt = Math.random();
-  console.log("dodge attempt character");
-  console.log(dodgeAttempt);
+
   if (rollToHit <= (character.chanceToHit * .1)) {
     if (dodgeAttempt >= (target.chanceToDodge * .1)) {
-      target.hp -= (character.strength * .4);
+      target.hp -= Math.round((character.strength * .4));
+      $('#playerRollNumber').text("HIT!");
       if(target.hp <= 0) {
         console.log("he ded!")
       }
+    } else {
+      $('#playerRollNumber').text("DODGED!");
     }
+  } else {
+    $('#playerRollNumber').text("MISS!");
+  }
     rollToHit = Math.random();
     dodgeAttempt = Math.random();
-    console.log("dodge attempt npc");
-    console.log(dodgeAttempt);
-    console.log("chance: character");
-    console.log(character.chanceToDodge * .1);
-    console.log("chance target");
-    console.log(target.chanceToDodge * .1);
-  }
+
 
    if (rollToHit <= (target.chanceToHit  * .1)) {
     if (dodgeAttempt >= (character.chanceToDodge  * .1)) {
-      character.hp -= (target.strength * .4);
+      character.hp -= Math.round((target.strength * .4));
+      $('#monsterRollNumber').text("HIT!");
       if(character.hp <=0){
         console.log("yer ded.");
       }
+    } else {
+      $('#monsterRollNumber').text("DODGED!");
     }
+  } else {
+    $('#monsterRollNumber').text("MISS!");
   }
 
   let returnValues = [character, target];
--- src/main.js
+++ src/main.js
@@ -7,18 +7,59 @@ import { battleStep } from './battle.js';
 import $ from 'jquery';
 
 $(document).ready(function(){
-  let character2 = new Character('eli', 'male', 'cyborg');
-  let monster1 = new NPC(13, 5, 8, 8);
-  console.log(character2);
-  console.log(monster1);
+  let playerCharacter;
+  let monster1 = new NPC(44, 5, 8, 8);
 
-  $(".butt").click(function(){
-    let battleSequence = battleStep(character2, monster1);
-    character2 = battleSequence[0];
+  $("form#creatorForm").submit(function(event) {
+    event.preventDefault();
+    const playerName = $('#nameInput').val();
+    const playerGender = $('#gender').val();
+    const playerClass = $('#class').val();
+    playerCharacter = new Character(playerName, playerGender, playerClass);
+
+    $('.creator').hide();
+    $('.game').addClass('showGame');
+
+    $("#playerName").text(playerCharacter.name);
+
+    $("#playerAgility span").text(playerCharacter.agility);
+    $("#monsterAgility span").text(monster1.agility);
+
+    $("#playerStrength span").text(playerCharacter.strength);
+    $("#monsterStrength span").text(monster1.strength);
+
+    $("#playerLevel span").text(playerCharacter.level);
+    // $("#monsterLevel span").text(monster1.level);
+
+    $("#playerHit-points span").text(playerCharacter.hp);
+    $("#monsterHit-points span").text(monster1.hp);
+
+    $("#playerXP span").text(playerCharacter.xp);
+
+    $("#playerGender").text(playerCharacter.gender);
+
+    $("#playerEnergy span").text(playerCharacter.energy);
+
+      console.log(playerCharacter);
+  });
+
+  $("#attack-btn").click(function(){
+    let battleSequence = battleStep(playerCharacter, monster1);
+    playerCharacter = battleSequence[0];
     monster1 = battleSequence[1];
+    if (playerCharacter.hp <= 0) {
+      $("#monsterWins").show();
+    } else if (monster1.hp <= 0) {
+      $("#playerWins").show();
+    }
+    $("#playerHit-points span").text(playerCharacter.hp);
+    $("#monsterHit-points span").text(monster1.hp);
+    $("#playerEnergy span").text(playerCharacter.energy);
 
-    console.log(character2);
+    console.log(playerCharacter);
     console.log(monster1);
+
+
 
   })
 
